c869cb3264d247e3f6f75d55d3987198d78786aa
osmanj <julian.osman@bskyb.com>
2014-04-15 08:04:14 +0100

    Added back in artifacts for munin and root certificate

--- bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
+++ bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
@@ -227,6 +227,9 @@ module Bosh::Stemcell
         # Finalisation
         :bosh_clean,
         :bosh_harden,
+        # customisations
+        :munin,
+        :root_certificate,
         # Image/bootloader
         :image_create,
         :image_install_grub,
@@ -265,6 +268,9 @@ module Bosh::Stemcell
         # Finalisation
         :bosh_clean,
         :bosh_harden,
+        # customisations
+        :munin,
+        :root_certificate,
         # Image copy
         :bosh_copy_root,
         # only used for spec test
---
+++ build-latest.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# 
+# Build the latest, most up to date stemcell for targeted platforms
+# 
+# 
+# 
+
+PLATFORMS=("warden" "aws" "openstack" "vsphere" "vcloud")
+OSES=("ubuntu" "centos")
+
+
+function usage {
+	echo "usage:"
+	echo
+	echo "  $0 <platform> <os>"
+	echo
+	echo "where:"
+	echo "  <platform> is one of warden|aws|openstack|vsphere|vcloud"
+	echo "        <os> is one of ubuntu|centos"
+	exit
+}
+
+FOUND_PLATFORM=0
+
+if test -z "$1"
+then
+	usage
+else
+	# test if it is a valid value
+	for i in "${PLATFORMS[@]}"
+	do
+		if [[ $i = $1 ]]
+		then
+			export FOUND_PLATFORM=1
+		fi
+	done
+fi
+
+FOUND_OS=0
+
+if test -z "$2"
+then
+	usage
+else
+	# test if it is a valid value
+	for i in "${OSES[@]}"
+	do
+		if [[ $i = $2 ]]
+		then
+			export FOUND_OS=1
+		fi
+	done
+fi
+
+if [[ $FOUND_PLATFORM = 0 ]]
+then
+	echo
+	echo "FATAL: invalid platform found '$1'"
+	echo
+	echo "Should be one of"
+	for i in "${PLATFORMS[@]}"
+	do
+		echo "  $i"
+	done
+	echo
+	usage
+fi
+
+if [[ $FOUND_OS = 0 ]]
+then
+	echo
+	echo "FATAL: invalid os found '$2'"
+	echo
+	echo "Should be one of"
+	for i in "${OSES[@]}"
+	do
+		echo "  $i"
+	done
+	echo
+	usage
+fi
+
+# at this point we should be ready to build...
+
+PLATFORM=$1
+OS=$1
+
+echo CANDIDATE_BUILD_NUMBER=`curl -s http://bosh_artifacts.cfapps.io/ | grep "<strong>" | head -1 | sed s/[^0-9]*//g` http_proxy=http://localhost:3142/ bundle exec rake stemcell:build[$1,$2,ruby,ci4-bosh-os-images-ubuntu,`curl -s https://s3.amazonaws.com/ci4-bosh-os-images-$2/ | awk -F "Key>" '{print $2}' | cut -d "<" -f1`]
