a6be906eca56b55a943ba0450694458380f85311
Ryan Grenz <info@ryangrenz.com>
2014-08-07 11:45:49 +0100

    Fixing drbd for trusty stemcell builds

--- stemcell_builder/stages/drbd/apply.sh
+++ stemcell_builder/stages/drbd/apply.sh
@@ -8,17 +8,24 @@ source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
 mkdir -p $chroot/$bosh_dir/src
-cp -r $dir/assets/drbd-8.4.4.tar.gz $chroot/$bosh_dir/src
+cp -r $dir/assets/drbd-8.4.5.tar.gz $chroot/$bosh_dir/src
+cp -r $dir/assets/drbd-utils-8.9.0.tar.gz $chroot/$bosh_dir/src
 
 pkg_mgr install lvm2
 
-run_in_bosh_chroot $chroot "
+run_in_bosh_chroot $chroot '
 cd src
-tar zxvf drbd-8.4.4.tar.gz
-cd drbd-8.4.4
-./configure --prefix=/ --with-km
-make KDIR=/lib/modules/3.0.0-32-virtual/build && make install
-depmod -a 3.0.0-32-virtual
-"
+tar zxvf drbd-8.4.5.tar.gz
+cd drbd-8.4.5
+make KDIR=/lib/modules/3.13.0-32-generic/build && make install
+depmod -a 3.13.0-32-generic
+cd ..
+tar zxvf drbd-utils-8.9.0.tar.gz
+cd drbd-utils-8.9.0
+./configure --prefix=/
+make && make install
+#cp scripts/drbd.conf /etc/drbd.conf
+#mkdir /etc/drbd.d
+'
 
 cp -v $dir/assets/global_common.conf $chroot/etc/drbd.d/
