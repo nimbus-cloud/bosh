6f592bcbba10235c55c972170c112502300ee709
Ben Corlett <ben.corlett@gmail.com>
2014-11-05 07:03:17 +0000

    Changes didn't work in first boot sadly. Moved to rc.local, where it looks all good

--- stemcell_builder/stages/base_ubuntu_firstboot/assets/root/firstboot.sh
+++ stemcell_builder/stages/base_ubuntu_firstboot/assets/root/firstboot.sh
@@ -6,7 +6,7 @@
 rm /etc/ssh/ssh_host*key*
 
 DISTRIB_CODENAME=$(lsb_release --codename | cut -f2)
-if [ $DISTRIB_CODENAME == "trusty" ]; then
+if [ "$DISTRIB_CODENAME" = "trusty" ]; then
   ifdown -a --no-loopback
   ifup -a --no-loopback
 else
@@ -16,10 +16,4 @@ fi
 
 dpkg-reconfigure -fnoninteractive -pcritical openssh-server
 dpkg-reconfigure -fnoninteractive sysstat
-
-# We've seen problems on stemcell replacements when the mac has changed but the ip remains the same.
-# The router caches the old mac address and doesn't forward new traffic to the new box until that
-# box has sent some traffic via it. This call makes sure the router is aware of it on startup.
-
-wget -O - -q -t 1 http://www.google.com > /dev/null 2>&1
 
