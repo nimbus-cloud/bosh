e6593318694ca0d9a384eb5cd424ef524f77c95c
Szlachta, Jacek <jacek.szlachta@sky.uk>
2015-10-05 09:51:48 +0100

    updated bat run script and config file.

--- run_bats.sh
+++ run_bats.sh
@@ -6,9 +6,10 @@ export BAT_DNS_HOST=10.76.247.230
 export BAT_DEPLOYMENT_SPEC=$PWD/bats-config.yml
 export BAT_INFRASTRUCTURE=vsphere
 export BAT_NETWORKING=manual
-export BAT_VCAP_PASSWORD=c1oudc0w
-#export BAT_VCAP_PASSWORD=OY2WGb4SrdXtnCyNaWl
-export BAT_STEMCELL=$PWD/tmp/bosh-stemcell-0000-vsphere-esxi-ubuntu-trusty-go_agent.tgz
+#export BAT_VCAP_PASSWORD=c1oudc0w
+export BAT_VCAP_PASSWORD=<nimbus_password>
+export BAT_STEMCELL=$PWD/tmp/bosh-stemcell-3087-vsphere-esxi-ubuntu-trusty-go_agent.tgz
 
 #bundle exec rake bat:env
+#bundle exec rake bat:net
 bundle exec rake bat
